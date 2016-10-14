1825068785fe33a03ea45b0d9a24663437bcd056
Radu Szasz <ras114@ic.ac.uk>
2016-10-14 17:37:07 +0100

    Add spring generated version of backend

---
+++ visible-server/src/main/java/com/visible/VisibleServerApplication.java
@@ -0,0 +1,12 @@
+package com.visible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisibleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisibleServerApplication.class, args);
+	}
+}
