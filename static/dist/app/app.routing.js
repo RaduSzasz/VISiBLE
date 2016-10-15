642969c97b64bf7bcac5ff771e6ab7bde6112236
Amey Kusurkar <amey1000@gmail.com>
2016-10-15 12:02:32 +0100

    Renamed frontend folder to static

---
+++ static/dist/app/app.routing.js
@@ -0,0 +1,10 @@
+"use strict";
+var router_1 = require('@angular/router');
+var appRoutes = [
+    {
+        path: '/',
+        component: AppComponent
+    }
+];
+exports.routing = router_1.RouterModule.forRoot(appRoutes);
+//# sourceMappingURL=app.routing.js.map
