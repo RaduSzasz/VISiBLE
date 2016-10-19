2680956aec485a6a25dff87bf7b62691a4588b7a
Jason Yu <jason.yu14@imperial.ac.uk>
2016-10-19 18:45:55 +0100

    backend accepts file on path /upload

---
+++ backend/src/main/java/com/visible/JavaProgram.java
@@ -0,0 +1,15 @@
+/* This class represents the uploaded Java files */
+package com.visible;
+
+public class JavaProgram{
+  private String code;
+
+  public JavaProgram(String code){
+    this.code = code;
+  }
+  
+  public String getCode(){
+    return this.code;
+  }
+
+}
