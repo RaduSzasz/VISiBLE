77b1e786add8d7c3e7f3b9ecd5eef7bf6b6bbac8
Amey Kusurkar <amey1000@gmail.com>
2016-11-25 15:20:26 +0000

    Added a primitive version of json object as response to get nextstate request

--- backend/src/main/java/com/visible/jpf/TreeInfo.java
+++ backend/src/main/java/com/visible/jpf/TreeInfo.java
@@ -1,9 +1,15 @@
 package com.visible.jpf;
 
+import gov.nasa.jpf.symbc.numeric.PathCondition;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class TreeInfo {
+
+    public List<State> getStatesToSend() {
+        return statesToSend;
+    }
 
     private List<State> statesToSend;
 
@@ -27,4 +33,19 @@ public class TreeInfo {
         return sb.toString();
     }
 
+    public String toJSON() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (State s: statesToSend) {
+            sb.append("{");
+            sb.append("id :" + s.getId() + ",\n");
+            sb.append("parent_ :" + s.getParent().getId() + ",\n");
+            s.getChildren().forEach(state -> sb.append(state.getId()));
+            PathCondition pc = s.getPc();
+            sb.append("pc :" + ((pc == null) ? "null" : pc.stringPC()));
+            sb.append("}\n");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
