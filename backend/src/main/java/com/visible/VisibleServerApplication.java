c86359686d20c5c60b43c54418844967182555ba
Radu Szasz <ras114@ic.ac.uk>
2017-01-01 22:07:43 +0200

    Do some cleanup

--- backend/src/main/java/com/visible/VisibleServerApplication.java
+++ backend/src/main/java/com/visible/VisibleServerApplication.java
@@ -1,17 +1,10 @@
 package com.visible;
 
-import com.visible.symbolic.jpf.JPFAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.concurrent.ExecutorService;
-
 @SpringBootApplication
 public class VisibleServerApplication {
-
-	private static int NUMBER_OF_THREADS = 1;
-	private static ExecutorService executor;
-	private static JPFAdapter adapter;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VisibleServerApplication.class, args);
