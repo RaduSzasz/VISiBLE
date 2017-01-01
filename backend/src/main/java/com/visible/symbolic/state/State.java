0ea3718e1bf35cd6bc6a29089bcc0944504ae5ef
Radu Szasz <ras114@ic.ac.uk>
2017-01-01 23:52:35 +0200

    Enhance State class with some useful stuff

--- backend/src/main/java/com/visible/symbolic/state/State.java
+++ backend/src/main/java/com/visible/symbolic/state/State.java
@@ -1,12 +1,14 @@
 package com.visible.symbolic.state;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import java.util.LinkedList;
 import java.util.List;
 
-public class State {
+public final class State {
 
     @JsonSerialize(using = ParentSerializer.class)
     private State parent;
@@ -15,12 +17,14 @@ public class State {
     private String elsePC;
     private int id;
 
-    public void setIfPC(String ifPC) {
+    public State setIfPC(String ifPC) {
         this.ifPC = ifPC;
+        return this;
     }
 
-    public void setElsePC(String elsePC) {
+    public State setElsePC(String elsePC) {
         this.elsePC = elsePC;
+        return this;
     }
 
     public int getId() {
@@ -41,10 +45,49 @@ public class State {
 
     public State(int id, State parent) {
         this.id = id;
-        this.children = new LinkedList<State>();
+        this.children = new LinkedList<>();
         this.parent = parent;
         this.ifPC = null;
         this.elsePC = null;
     }
 
+    public State(int id, State parent, String ifPC, String elsePC) {
+        this.id = id;
+        this.children = new LinkedList<>();
+        this.parent = parent;
+        this.ifPC = ifPC;
+        this.elsePC = elsePC;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        State state = (State) o;
+
+        if (id != state.id) return false;
+        if (parent != null ? !parent.equals(state.parent) : state.parent != null) return false;
+        if (ifPC != null ? !ifPC.equals(state.ifPC) : state.ifPC != null) return false;
+        return elsePC != null ? elsePC.equals(state.elsePC) : state.elsePC == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = parent != null ? parent.hashCode() : 0;
+        result = 31 * result + (ifPC != null ? ifPC.hashCode() : 0);
+        result = 31 * result + (elsePC != null ? elsePC.hashCode() : 0);
+        result = 31 * result + id;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        try {
+            return new ObjectMapper().writer().withDefaultPrettyPrinter().writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            return null;
+        }
+    }
 }
