6f09878e7dbf3a6635de46731a3ede51b09e09d3
Feroz Jilla <ferozjilla@gmail.com>
2017-01-02 02:49:19 +0000

    Change systemjs, remove typings dependency, update package.json with latest angular, add testing config and template files. Test config files to be fully set up.

--- frontend/systemjs.config.js
+++ frontend/systemjs.config.js
@@ -1,10 +1,72 @@
 /**
- * System configuration for Angular 2 samples
+ * System configuration for Angular samples
  * Adjust as necessary for your application needs.
  */
+(function (global) {
+  System.config({
+    paths: {
+      // paths serve as alias
+      'npm:': 'node_modules/'
+    },
+    // map tells the System loader where to look for things
+    map: {
+      // our app is within the app folder
+      app: 'dist',
+
+      // angular bundles
+      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+
+      // other libraries
+      'rxjs':                      'npm:rxjs',
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+      'ng2-uploader':              'npm:ng2-uploader',
+      'd3':                        'npm:d3'
+    },
+    // packages tells the System loader how to load when no filename and/or no extension
+    packages: {
+      app: {
+        main: './main.js',
+        defaultExtension: 'js'
+      },
+      rxjs: {
+        defaultExtension: 'js'
+      },
+      'ng2-uploader': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      'd3': {
+        main: 'd3.js',
+        defaultExtension: 'js'
+      }
+    }
+  });
+})(this);
+
+
+
+
+
+
+/**
+ *
+ * System configuration for Angular 2 samples
+ * Adjust as necessary for your application needs.
 (function(global) {
+  System.config({
+    paths: {
+      // paths serve as alias
+      'npm:': 'node_modules/'
+    },
   // map tells the System loader where to look for things
-  var map = {
+  var map : {
     'app':                        'dist',
     '@angular':                   'node_modules/@angular',
     'ng2-uploader':               'node_modules/ng2-uploader',
@@ -49,4 +111,4 @@
     packages: packages
   };
   System.config(config);
-})(this);
+})(this);*/
