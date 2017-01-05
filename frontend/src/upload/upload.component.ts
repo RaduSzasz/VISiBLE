24fbc51d5f98427f140f614cbcb633f41bb65199
Jason Yu <ycm.jason@gmail.com>
2017-01-06 05:55:26 +0800

    added checkboxes to select arguments and adapted the new protocol with backend

--- frontend/src/upload/upload.component.ts
+++ frontend/src/upload/upload.component.ts
@@ -4,7 +4,7 @@ import { TreeService }   from '../tree/tree.service';
 import { Tree } from '../tree/tree';
 import { TreeComponent } from '../tree/tree.component';
 
-import { Method, Arg } from './method';
+import { Method } from './method';
 import { ModalDirective } from 'ng2-bootstrap';
 
 @Component({
@@ -20,24 +20,45 @@ export class UploadComponent {
     url: 'http://localhost:8080/upload'
   };
   symbolicMethod : Method = null;
-  methods: Method[] = [];
+
+  jar = null;
+  methods = null;
+  isSymb = null;
 
   constructor(private treeService: TreeService){ }
 
   selectSymbolic(data) {
-    /* TODO: Get methods from data.response */
-    if (data && data.response) {
-      this.methods = [
-        new Method("symVis", [new Arg("int", "num"), new Arg("int", "num2"), new Arg("int", "num3"), new Arg("int", "num4")]),
-        new Method("method1", [])
-      ];
-      //this.methods = JSON.parse(data.response);
-      this.staticModal.show();
-    }
+    if(!data || !data.response) return;
+    let res = JSON.parse(data.response);
+
+    if(res.error) throw res.error;
+    this.jar = res.jar;
+    let flatten = (arr) => [].concat.apply([], arr);
+    this.methods = flatten(res.data.map((cls) => {
+      return cls.methods.map((method) => {
+        return new Method(cls.class,
+                          method.name,
+                          method.numArgs,
+                          method.signature);
+      });
+    }));
+    this.staticModal.show();
+  }
+
+  isSymbSetup() {
+    if(!this.symbolicMethod) return;
+    this.isSymb = [];
+    for(let i = 0; i < this.symbolicMethod.numArgs; i++){
+      this.isSymb.push({value: true});
+    };
   }
 
   setInitialTree(){
-    let steer_promise : Promise<Tree> = this.treeService.drawTree(this.symbolicMethod);
+    let steer_promise: Promise<Tree> = this.treeService.drawTree(
+      this.jar,
+      this.symbolicMethod,
+      this.isSymb.map(s => s.value)
+    );
     steer_promise.then(tree => {
       this.onUpload.emit(tree);
     });
