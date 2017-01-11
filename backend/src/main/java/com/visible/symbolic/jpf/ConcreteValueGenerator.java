502658c1a0af53225ccbf3606601f15faaf33cda
Amey Kusurkar <amey1000@gmail.com>
2017-01-11 16:07:18 +0000

    Set up template for solver

---
+++ backend/src/main/java/com/visible/symbolic/jpf/ConcreteValueGenerator.java
@@ -0,0 +1,17 @@
+package com.visible.symbolic.jpf;
+
+import java.util.Map;
+
+public class ConcreteValueGenerator<T> {
+
+    public ConcreteValueGenerator() {
+
+    }
+
+    public void addConstraint(String constraint) {
+    }
+
+    public Map<String, T> getConcreteValues() {
+        return null;
+    }
+}
